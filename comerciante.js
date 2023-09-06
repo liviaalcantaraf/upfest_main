df9a35e497f0cef4e87bda9f5cd718050f4a14c7
liviaalcantaraf <liviaalcantaraf@gmail.com>
2023-09-06 01:18:03 +0100

    final

--- comerciante.js
+++ comerciante.js
@@ -2,73 +2,75 @@ const {queryDB} = require("./connection.js");
 const express = require("express");
 const router = express.Router({mergeParams: true});
 
-router.post("/comerciantes/criar", async function (req, res) {
-    try {
-        const [eventoExiste] = await queryDB(
-            "SELECT id FROM evento WHERE id = ?",
-            [req.params.id_evento]
-        );
-        if (!eventoExiste) {
-            return res.status(400).json({message: 'É necessário relacionar o comerciante a um evento.'});
-        } else {
-            let designacao = req.body.designacao;
-            let criarComerciante = await queryDB("INSERT INTO comerciante(evento, designacao) values (?, ?)", [req.params.id_evento, designacao]);
-            res.status(200).json(criarComerciante);
+
+    router.post("/comerciantes/criar", async function (req, res) {
+        try {
+            const [eventoExiste] = await queryDB(
+                "SELECT id FROM evento WHERE id = ?",
+                [req.params.id_evento]
+            );
+            if (!eventoExiste) {
+                throw new Error('É necessário relacionar o comerciante a um evento.');
+            } else {
+                let designacao = req.body.designacao;
+                let criarComerciante = await queryDB("INSERT INTO comerciante(evento, designacao) values (?, ?)", [req.params.id_evento, designacao]);
+                res.status(200).json(criarComerciante);
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(400).send(error.message);
         }
-    } catch (e) {
-        console.error(error);
-        res.status(500).json({message: 'Ocorreu um erro ao criar o comerciante.'});
-
-    }
-
-
-});
+    });
 
 
 router.post("/comerciantes/:id_comerciante/editar", async function (req, res) {
     try {
-
         const [eventoExiste] = await queryDB(
             "SELECT id FROM evento WHERE id = ?",
             [req.params.id_evento]
         );
 
-        if (eventoExiste) {
-            let novaDesignacao = req.body;
-            let designacao = req.body;
-
-            if (novaDesignacao.length > 0) {
-                // Verificar se novaDesignacao tem um valor definido
-                let atualizarComerciante = await queryDB("UPDATE comerciante SET designacao = ? WHERE designacao = ? ", [novaDesignacao, designacao]);
-
-                res.status(200).json(atualizarComerciante);
-            } else {
-                res.status(400).json({
-                    message: "O campo 'designacao' deve ser fornecido para a atualização."
-                });
-            }
+        if (!eventoExiste) {
+            throw new Error('É necessário relacionar o comerciante a um evento.');
         }
-    } catch (e) {
-        res.status(400).json({
-            message: "Não foi possível atualizar o comerciante"
-        });
+
+        let novaDesignacao = req.body;
+        let designacao = req.body;
+
+        if (novaDesignacao.length > 0) {
+            // Verificar se novaDesignacao tem um valor definido
+            let atualizarComerciante = await queryDB("UPDATE comerciante SET designacao = ? WHERE designacao = ? ", [novaDesignacao, designacao]);
+
+            res.status(200).json(atualizarComerciante);
+        } else {
+            throw new Error("O campo 'designacao' deve ser fornecido para a atualização.");
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 });
 
 
 router.post("/registar_compra", async function (req, res) {
     try {
-        let {participante, produto: id_produto, quantidade} = req.body;
-        let {id_evento} = req.params;
+        let { participante, produto: id_produto, quantidade } = req.body;
+
+
+// necessidade de preencher todos os campos do registo de compra
+        if (!participante || !id_produto || !quantidade) {
+            throw new Error("Para registar a compra, é necessário que todos os campos estejam preenchidos.");
+        }
+
 
         // Validar se existe alguma conta_cashless do participante nesse evento;
+        let { id_evento } = req.params;
         let [conta] = await queryDB(
-            "SELECT cc.id FROM participante p INNER JOIN conta_cashless cc ON p.id = cc.id WHERE cc.evento = ? AND p.email = ?",
+            "SELECT c.id FROM participante p INNER JOIN conta_cashless c ON p.id = c.id WHERE c.evento = ? AND p.email = ?",
             [id_evento, participante]
         );
 
         if (!conta) {
-            return res.status(404).send("A conta não existe. Insira uma conta válida");
+            throw new Error("A conta não existe. Insira uma conta válida");
         }
 
         let [verificarProduto] = await queryDB(
@@ -77,37 +79,34 @@ router.post("/registar_compra", async function (req, res) {
         );
 
         if (!verificarProduto) {
-            return res.status(404).send("O produto indicado não foi encontrado.");
+            throw new Error("O produto indicado não foi encontrado.");
         }
-
 
         let [produto] = await queryDB("SELECT valor FROM produto_comerciante WHERE id = ?", [id_produto]);
         let [saldo] = await queryDB("SELECT valor_atual FROM conta_cashless WHERE id = ?", [conta.id]);
-        // Calcular valor da compra que está a ser feita (considerar valor do produto e quantidade indicada)
 
-
+        // Calcular valor da compra que está sendo feita (considerando valor do produto e quantidade indicada)
         let valorCompra = produto.valor * quantidade;
 
         if (valorCompra > saldo.valor_atual) {
-            return res.status(400).send("Saldo insuficiente.");
+            throw new Error("Saldo insuficiente.");
         }
 
         let novoSaldo = saldo.valor_atual - valorCompra;
         let data = Date.now();
 
-        // Registar o movimento cashless do tipo "gasto"
+        // Registar movimento
         let movimento = await queryDB("INSERT INTO movimento_cashless (tipo, conta, valor, saldo, data) VALUES (?, ?, ?, ?, ?)",
             ["gasto", conta.id, valorCompra, novoSaldo, data]);
 
-        // Registar o gasto cashless para o produto comprado e a quantidade
+        // Registo de gasto
         await queryDB("INSERT INTO gasto_cashless (movimento, produto, quantidade, valor_unitario) VALUES (?, ?, ?, ?)",
             [movimento.insertId, id_produto, quantidade, produto.valor]);
 
         // Atualizar o saldo atual na tabela conta_cashless
         await queryDB("UPDATE conta_cashless SET valor_atual = ? WHERE id = ?", [novoSaldo, conta.id]);
 
-
-       // sucesso de compra
+        // Sucesso de compra
 
         const produtosComprados = [{
             id_produto,
@@ -120,44 +119,39 @@ router.post("/registar_compra", async function (req, res) {
             produtosComprados
         });
     } catch (error) {
-        res.status(500).json({
-            message: "Ocorreu um erro ao registar sua compra. Tente novamente."
-        });
+
         res.status(400).send(error.message);
     }
 });
 
-
 router.get('/comerciantes/listar', async function (req, res) {
 
     try {
-        // verificação de id_evento
+        // Verificação de id_evento
         let [eventoExiste] = await queryDB(
             "SELECT id FROM evento WHERE id = ?",
             [req.params.id_evento]);
 
         if (!eventoExiste) {
-            return res.status(400).json({error: "Não existem eventos para listar comerciantes."});
+            throw new Error("Não existem eventos para listar comerciantes.");
         }
 
         // Verificação de comerciantes
         let [comercianteExiste] = await queryDB("SELECT id FROM comerciante");
 
         if (comercianteExiste.length === 0) {
-            return res.status(400).json({error: "Não existem comerciantes para listar."});
+            throw new Error("Não existem comerciantes para listar.");
         }
 
-        // Se existirem evento e comerciantes, listar os comerciantes
+        // Se existirem eventos e comerciantes, listar os comerciantes
 
         let listaComerciantes = await queryDB("SELECT id as id_comerciante, evento as evento, designacao as nome_comerciante FROM comerciante");
 
         res.status(200).json(listaComerciantes);
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({error: "Ocorreu um erro ao listar comerciantes."});
+    } catch (error) {
+
+        res.status(400).send(error.message);
     }
-
-
 });
 
 module.exports = router;
--- produto.js
+++ produto.js
@@ -4,45 +4,36 @@ const router = express.Router({mergeParams: true});
 
 
 router.post("/criar", async function (req, res) {
+    try {
+        // Validação de id_evento e id_comerciante
+        const [eventoExiste] = await queryDB(
+            "SELECT id FROM evento WHERE id = ?",
+            [req.params.id_evento]
+        );
 
+        const [comercianteExiste] = await queryDB(
+            "SELECT id FROM comerciante WHERE id = ?",
+            [req.params.id_comerciante]
+        );
 
-        try {
-//validação de id_evento e id_comerciante
-            const [eventoExiste] = await queryDB(
-                "SELECT id FROM evento WHERE id = ?",
-                [req.params.id_evento]
-            );
-
-            const [comercianteExiste] = await queryDB(
-                "SELECT id FROM comerciante WHERE id = ?",
-                [req.params.id_comerciante]
-            );
-
-
-            if (!comercianteExiste) {
-                return res.status(400).json({message: 'É necessário relacionar o produto a um comerciante.'});
-            }
-            if (!eventoExiste) {
-                return res.status(400).json({message: 'É necessário relacionar o produto a um evento.'});
-            } else {
-                let {designacao, valor} = req.body;
-                let comerciante = req.params.id_comerciante;
-
-                let criarProduto = await queryDB("INSERT INTO produto_comerciante( comerciante, designacao, valor) values (?, ?, ?)", [comerciante, designacao, valor]);
-                res.status(200).json(criarProduto);
-            }
-
-
-        } catch
-            (e) {
-            console.error(e)
-            res.json({error: "Não foi possível criar produto, tente novamente."});
+        if (!comercianteExiste) {
+            throw new Error('É necessário relacionar o produto a um comerciante.');
         }
-        console.log(req.params.id_comerciante);
+        if (!eventoExiste) {
+            throw new Error('É necessário relacionar o produto a um evento.');
+        } else {
+            let { designacao, valor } = req.body;
+            let comerciante = req.params.id_comerciante;
 
+            let criarProduto = await queryDB("INSERT INTO produto_comerciante(comerciante, designacao, valor) values (?, ?, ?)", [comerciante, designacao, valor]);
+            res.status(200).json(criarProduto);
+        }
+    } catch (error) {
+
+        res.status(400).send(error.message);
     }
-)
-;
+});
+
 
 router.post("/:id_produto/editar", async function (req, res) {
     try {
@@ -60,76 +51,69 @@ router.post("/:id_produto/editar", async function (req, res) {
             "SELECT id FROM produto_comerciante WHERE id = ?",
             [req.params.id_produto]
         );
-        //validação de evento, comerciante e produto
+
+        // Validação de evento, comerciante e produto
         if (!eventoExiste) {
-            return res.status(400).json({message: 'É necessário relacionar o produto a um evento.'});
+            throw new Error('É necessário relacionar o produto a um evento.');
         }
 
         if (!comercianteExiste) {
-            return res.status(400).json({message: 'É necessário relacionar o produto a um comerciante.'});
+            throw new Error('É necessário relacionar o produto a um comerciante.');
         }
 
         if (!produtoExiste) {
-            return res.status(400).json({message: 'Para editar um produto é necessário indicar qual será editado.'});
+            throw new Error('Para editar um produto é necessário indicar qual será editado.');
         } else {
-            let {novoComerciante, novaDesignacao, novoValor} = req.body;
-            let {comerciante, designacao, valor} = req.body;
+            let { novoComerciante, novaDesignacao, novoValor } = req.body;
+            let { comerciante, designacao, valor } = req.body;
 
             let atualizarProduto = await queryDB("UPDATE produto_comerciante SET  designacao = ?, valor = ? WHERE  designacao = ? AND valor = ?", [novaDesignacao, novoValor, designacao, valor]);
 
             res.status(200).json(atualizarProduto);
         }
+    } catch (error) {
 
-
-    } catch (e) {
-        console.error(e)
-        res.json({error: "Não foi possível atualizar o produto, tente novamente."});
+        res.status(400).send(error.message);
     }
 });
 
 
 router.get('/listar', async function (req, res) {
-
-
     try {
         // Verificação de evento
         const [eventoExiste] = await queryDB(
             "SELECT id FROM evento WHERE id = ?",
-            [req.params.id_evento]);
+            [req.params.id_evento]
+        );
 
         if (!eventoExiste) {
-            return res.status(400).json({error: "Não existem eventos para listar produtos."});
+            throw new Error('Não existem eventos para listar produtos.');
         }
 
         const [comercianteExiste] = await queryDB(
             "SELECT id FROM comerciante WHERE id = ?",
-            [req.params.id_comerciante]);
+            [req.params.id_comerciante]
+        );
 
         if (!comercianteExiste) {
-            return res.status(400).json({error: "Não existem comerciantes"});
+            throw new Error('Não existem comerciantes.');
         }
 
         // Verificação de produtos
         const [produtoExiste] = await queryDB("SELECT id FROM produto");
 
         if (comercianteExiste.length === 0) {
-            return res.status(400).json({error: "Não existem produtos para listar"});
-
+            throw new Error('Não existem produtos para listar.');
         } else {
             const produtos = await queryDB(`SELECT *
                                             FROM produto_comerciante`);
 
             res.status(200).json(produtos);
         }
+    } catch (error) {
 
-    } catch (e) {
-        console.error(error);
-        res.status(500).json({message: 'Ocorreu um erro ao listar os produtos.'});
-
+        res.status(400).send(error.message);
     }
-
-
 });
-
 
 module.exports = router;
